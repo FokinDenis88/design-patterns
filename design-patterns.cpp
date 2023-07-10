af33b2edcc3e01302336cd56391a3b88944122fc
FokinDenis88 <fokin-denis-88@yandex.ru>
2023-07-10 14:57:43 +0400

    First structure of project

---
+++ design-patterns.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "command.hpp"
+
+
+int main() {
+    std::cout << "Hello World!\n";
+}
---
+++ include/command.hpp
@@ -0,0 +1,14 @@
+#ifndef COMMAND_HPP
+#define COMMAND_HPP
+
+
+/** Software Design Patterns */
+namespace pattern {
+	namespace creational {
+		void Command() {
+
+		}
+	}
+}
+
+#endif // !COMMAND_HPP
