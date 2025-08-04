ed99ddc6e51a564637cc514fb4fcd886aef8daad
FokinDenis88 <fokin-denis-88-code@ya.ru>
2025-08-04 20:36:35 +0400

    feat: non virtual interface idiom (nvi)

--- include/cpp-idiom/non-virtual-interface.hpp
+++ include/cpp-idiom/non-virtual-interface.hpp
@@ -7,24 +7,67 @@
 /** C++ Idioms */
 namespace pattern {
 	namespace cpp_idiom {
+
+		/** Non-Virtual Interface (NVI) */
 		namespace non_virtual_interface {
-			// https://en.cppreference.com/w/cpp/language/pimpl
+			// https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Non-Virtual_Interface
+			// https://en.wikipedia.org/wiki/Non-virtual_interface_pattern
 
-			class Widget {
+			/**
+			* This pattern is closer to pattern Templated Method.
+			*
+			* The C++ Non-Virtual Interface (NVI) pattern is a design idiom that uses public non-virtual functions in a base class to
+			* call private or protected virtual functions. This approach provides a controlled and consistent interface for clients
+			* while allowing derived classes to customize specific behaviors through the virtual functions.
+			*
+			* Encapsulation and Control:
+			* The NVI pattern encapsulates the implementation details and provides a level of control over
+			* how derived classes can modify behavior. The base class can perform pre- and post-operations around
+			* the virtual function calls, ensuring invariants or managing resources.
+			*
+			* Reduced Complexity for Clients:
+			* Clients only need to interact with the public non-virtual interface, simplifying their usage and reducing
+			* their dependency on the specific implementation details of derived classes.
+			*
+			* Relationship to Template Method Pattern:
+			* The NVI pattern is closely related to the Template Method design pattern, where an algorithm's structure is
+			* defined in a base class, and specific steps of the algorithm are left to be implemented by derived classes.
+			*/
+
+
+			class Base {
 			public:
-				// public data and functions
+				/**
+				* Public Non-Virtual Interface:
+				* The functions that clients interact with are public and non-virtual. These functions define the stable
+				* and consistent interface of the base class.
+				*/
+				void DoSomething() { // Публичный интерфейс
+					Prepare();
+					DoSomethingImpl(); // Вызываем виртуальный метод
+					Cleanup();
+				}
 
-				// All public, protected, and virtual members remain in the interface class
+			protected:
+				/*
+				* Private/Protected Virtual Implementation:
+				* Inside the public non-virtual functions, calls are made to private or protected virtual functions.
+				* These virtual functions are the customization points for derived classes.
+				*/
+				virtual void DoSomethingImpl() = 0; // Защищённый виртуальный метод
 
 			private:
-				// All private data members and all private non - virtual member functions are placed in the implementation class.
+				void Prepare() {
+				}
 
-				/** Unspecified T */
-				struct Impl;
-
-				/** Opaque Pointer to Unspecified T */
-				std::unique_ptr<Impl> pImpl_;
+				void Cleanup() {
+				}
 			};
+
+			class Derived : public Base {
+            protected:
+                void DoSomethingImpl() override { /* ... */ }
+            };
 
 		} // !namespace non_virtual_interface
 
